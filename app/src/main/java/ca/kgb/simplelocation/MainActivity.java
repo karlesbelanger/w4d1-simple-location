eb74b9dd61777a8d1f439a818e9e841bc3ddd2c0
karlesbelanger <karlesbelanger@gmail.com>
2016-08-10 02:32:16 -0400

    Completed on gps update homework

--- app/src/main/java/ca/kgb/simplelocation/MainActivity.java
+++ app/src/main/java/ca/kgb/simplelocation/MainActivity.java
@@ -1,9 +1,13 @@
 package ca.kgb.simplelocation;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -12,6 +16,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
@@ -19,11 +25,18 @@ import com.google.android.gms.location.LocationServices;
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+
+    private LocationListener mLocationListener;
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int REQUEST_RESOLVE_ERROR = 1;
     private static final int REQUEST_LOCATION = 1;
     private GoogleApiClient mGoogleApiClient;
     private boolean mResolvingError;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +44,55 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_main);
         mGoogleApiClient = buildGoogleApiClient();
         mGoogleApiClient.connect();
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(final Location location) {
+                updateLocation(location);
+                Log.d(TAG, "onLocationChanged: (Long, Lat) : ("
+                        + location.getLongitude() + ", " + location.getLatitude() + ")");
+            }
+
+            @Override
+            public void onStatusChanged(final String provider,
+                                        final int status, final Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(final String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(final String provider) {
+            }
+        };
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private void updateLocation(Location location) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, mLocationListener);
     }
 
     protected synchronized GoogleApiClient buildGoogleApiClient() {
@@ -91,6 +153,7 @@ public class MainActivity extends AppCompatActivity implements
             mResolvingError = true;
         }
     }
+
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -105,7 +168,7 @@ public class MainActivity extends AppCompatActivity implements
                     LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         }
 
-    Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         Log.d(TAG, "onConnected: " + location.getLatitude() + " : " + location.getLongitude());
     }
 
@@ -114,4 +177,23 @@ public class MainActivity extends AppCompatActivity implements
 
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://ca.kgb.simplelocation/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
